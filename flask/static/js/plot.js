5023747605359735de85612f4c7473cd0fa99d56
adaniel99 <adaniel99@vt.edu>
2020-10-29 18:39:24 -0400

    scripting for the plot button

---
+++ flask/static/js/plot.js
@@ -0,0 +1,40 @@
+window.addEventListener('load', () => {
+//    const add_form = document.getElementById("add_form");
+//    add_form.addEventListener('submit', e => {
+//	e.preventDefault();
+//	const path = new URL(window.location).pathname.split('/'); // ["", "sim", "uuid"]
+//	const id = path[path.length - 2];
+//	const name = e.target[0].value;
+//	const inpFile = editor.getValue();
+//	fetch(`http://${window.location.host}/getplot`, { //fetch API, sending info to this url with optional parameters
+//	    method: 'POST',
+//	    headers: {
+//		"content-type":"application/json"
+//	    },
+//	    body: JSON.stringify({
+//		id,
+//		name//,
+//		inpFile
+//	    }),
+//	    redirect: 'manual'
+//	})
+//	    .then(result => result.json())
+  //  });
+
+
+    const plot = document.getElementById("plot")
+    run.addEventListener('click', () => {
+	const path = new URL(window.location).pathname.split('/');
+	const id = path[path.length - 2];
+	fetch(`http://${window.location.host}/plot`, {
+	    method: 'POST',
+	    headers: {
+		"content-type":"application/json"
+	    },
+	    body: JSON.stringify({
+		id
+	    }),
+	    redirect: 'manual'
+	})
+    });
+});
